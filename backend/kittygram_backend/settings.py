71b3cf768feaef94ac4dd8b78dd09b732c28fb09
Raider <dimentr631@mail.ru>
2024-11-22 19:28:22 +0400

    Update settings. dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,15 @@
-# flake8: noqa
 import os
 from pathlib import Path
 
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['raidert.work.gd']
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
